8ec72016e40eb242c53b817f51bfe44c666c1d28
lukas-schmdt <lukas.schmidt1812@gmail.com>
2026-04-11 00:58:41 +0200

    Splashscreen fix.

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,4 +1,11 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-_flutter.loader.load();
+// Drive the Flutter loader manually so we can suppress the default loading
+// indicator and let our HTML splash screen (in index.html) own the loading UI.
+_flutter.loader.load({
+  onEntrypointLoaded: async function (engineInitializer) {
+    const appRunner = await engineInitializer.initializeEngine();
+    await appRunner.runApp();
+  },
+});
